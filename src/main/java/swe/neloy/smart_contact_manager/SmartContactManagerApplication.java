0390242ab3810b915a90b009174bac0671596595
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-08-10 23:19:21 +0600

    database and jpa configuration added

--- src/main/java/swe/neloy/smart_contact_manager/SmartContactManagerApplication.java
+++ src/main/java/swe/neloy/smart_contact_manager/SmartContactManagerApplication.java
@@ -2,12 +2,13 @@ package swe.neloy.smart_contact_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SmartContactManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartContactManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmartContactManagerApplication.class, args);
+    }
 
 }
