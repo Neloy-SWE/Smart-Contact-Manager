8fa83f8d34cabc390c34beeb627cf62483342079
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-08-10 23:19:43 +0600

    contact entity added

---
+++ src/main/java/swe/neloy/smart_contact_manager/entities/Contact.java
@@ -0,0 +1,32 @@
+/* 
+Created by Neloy on 10 August, 2025.
+Email: taufiqneloy.swe@gmail.com
+*/
+
+package swe.neloy.smart_contact_manager.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "CONTACT")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int contactId;
+    private String name;
+    private String secondName;
+    private String work;
+    private String email;
+    private String phone;
+    private String image;
+
+    @Column(length = 500)
+    private String description;
+}
