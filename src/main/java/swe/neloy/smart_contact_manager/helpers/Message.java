ed4d2a0a882b5e9df96d45a6c94e2e8d500a2689
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-08-13 02:11:51 +0600

    alert message model added

---
+++ src/main/java/swe/neloy/smart_contact_manager/helpers/Message.java
@@ -0,0 +1,18 @@
+/* 
+Created by Neloy on 12 August, 2025.
+Email: taufiqneloy.swe@gmail.com
+*/
+
+package swe.neloy.smart_contact_manager.helpers;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+    private String content;
+    private String type;
+}
