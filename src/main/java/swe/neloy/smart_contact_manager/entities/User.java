b8f06d707f84158ec838bc4d97754c77e4ae1a14
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-08-14 00:32:58 +0600

    user validation setted

--- src/main/java/swe/neloy/smart_contact_manager/entities/User.java
+++ src/main/java/swe/neloy/smart_contact_manager/entities/User.java
@@ -6,6 +6,10 @@ Email: taufiqneloy.swe@gmail.com
 package swe.neloy.smart_contact_manager.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -23,15 +27,31 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @NotBlank(message = "Name field is required!")
+    @Size(min = 2, max = 20, message = "Name must be between 2 and 20 characters!")
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "Name can only contain letters and spaces!")
     private String name;
 
+    @NotBlank(message = "Email field is required!")
+    @Email(message = "Invalid email format!")
+    @Size(max = 50, message = "Email must not exceed 50 characters!")
     @Column(unique = true)
     private String email;
+
+    @NotBlank(message = "Password is required!")
+    @Size(min = 8, message = "Password must be at least 8 characters long!")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "Password must contain upper, lower, number, and special character!"
+    )
     private String password;
+
     private String role;
     private boolean enabled;
     private String imageUrl;
 
+    @Size(max = 500, message = "About section must not exceed 500 characters!")
     @Column(length = 500)
     private String about;
 
